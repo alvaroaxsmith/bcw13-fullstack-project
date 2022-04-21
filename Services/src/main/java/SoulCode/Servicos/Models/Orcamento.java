357a5aaedb2e8d7e6cf24494be0a145e63f5f353
giovaner10 <giovanerr10@gmail.com>
2022-04-21 19:41:51 -0300

    Orcamento.java, tabela de orcamento com a relacao @OneToOne

--- Services/src/main/java/SoulCode/Servicos/Models/Orcamento.java
+++ Services/src/main/java/SoulCode/Servicos/Models/Orcamento.java
@@ -30,8 +30,53 @@ public class Orcamento {
 	@OneToOne
 	@JoinColumn(name = "id_servico")
 	private Servico servico;
-	
 
 
-	
+	public Integer getIdOrcamento() {
+		return idOrcamento;
+	}
+
+	public void setIdOrcamento(Integer idOrcamento) {
+		this.idOrcamento = idOrcamento;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public FormaPagamento getFormaPagamento() {
+		return formaPagamento;
+	}
+
+	public void setFormaPagamento(FormaPagamento formaPagamento) {
+		this.formaPagamento = formaPagamento;
+	}
+
+	public StatusPagamento getStatusPagamento() {
+		return statusPagamento;
+	}
+
+	public void setStatusPagamento(StatusPagamento statusPagamento) {
+		this.statusPagamento = statusPagamento;
+	}
+
+	public Servico getServico() {
+		return servico;
+	}
+
+	public void setServico(Servico servico) {
+		this.servico = servico;
+	}
 }
