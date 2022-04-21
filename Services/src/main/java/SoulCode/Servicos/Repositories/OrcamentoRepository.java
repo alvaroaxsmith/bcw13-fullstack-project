b548e88d08d1eadcc6df9ce67a5b79623a236aae
giovaner10 <giovanerr10@gmail.com>
2022-04-21 19:57:07 -0300

    Para ser usado no crud de orcamento. Service, e na sequencia repositorio.

---
+++ Services/src/main/java/SoulCode/Servicos/Repositories/OrcamentoRepository.java
@@ -0,0 +1,13 @@
+package SoulCode.Servicos.Repositories;
+
+import SoulCode.Servicos.Models.Funcionario;
+import SoulCode.Servicos.Models.Orcamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrcamentoRepository extends JpaRepository<Orcamento,Integer>{
+	
+	
+
+}
