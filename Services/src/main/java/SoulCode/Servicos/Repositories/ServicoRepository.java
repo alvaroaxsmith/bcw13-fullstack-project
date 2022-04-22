b8db38b8273a26152e1be8edbca37e0fb749edc5
giovaner10 <giovanerr10@gmail.com>
2022-04-21 22:36:19 -0300

    cards 52 até 57. ServicoRepository.java.

--- Services/src/main/java/SoulCode/Servicos/Repositories/ServicoRepository.java
+++ Services/src/main/java/SoulCode/Servicos/Repositories/ServicoRepository.java
@@ -2,16 +2,39 @@ package SoulCode.Servicos.Repositories;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import SoulCode.Servicos.Models.Servico;
 import org.springframework.data.jpa.repository.Query;
 
-import SoulCode.Servicos.Models.Funcionario;
-import SoulCode.Servicos.Models.Servico;
 
 public interface ServicoRepository extends JpaRepository<Servico,Integer>{
-	
-	
 
+
+	//Para a tabela de Serviços, criar um método get pelo Id do Funcionário e a data de Entrada do serviço #77
+	@Query(value = "select * from servico WHERE id_funcionario = :idFuncionario and  data_entrada = :data", nativeQuery = true)
+	List<Servico> card52(Integer idFuncionario, Date data);
+
+	//Para a tabela de Serviços, criar um método get para buscar os serviços de determinada para frente #78
+	@Query(value = "select * from servico WHERE data_entrada between :dataInicio and CURRENT_DATE()", nativeQuery = true)
+	List<Servico> card53(Date dataInicio);
+
+	//Para a tabela de Serviços, criar um método get para buscar todos os serviços concluídos de um determinado funcionario #79
+	@Query(value = "select * from servico WHERE id_funcionario = :idFuncionario and status = 'CONCLUIDO'", nativeQuery = true)
+	List<Servico> card54(Integer idFuncionario);
+
+	//Para a tabela de Serviços, criar um método get para buscar todos os serviços concluídos dentro de um intervalo de datas #80
+	@Query(value = "select * from servico WHERE status = 'CONCLUIDO'", nativeQuery = true)
+	List<Servico> card55();
+
+	//Para a tabela de Serviço, criar um método get para buscar todos os serviços concluídos de um determinado funcionário em um determinado períodi #81
+	@Query(value = "select * from servico WHERE id_funcionario = :idFuncionario and (data_entrada between :dataInicio and :dataFinal) and status = 'CONCLUIDO'", nativeQuery = true)
+	List<Servico> card56(Integer idFuncionario, Date dataInicio, Date dataFinal);
+
+
+	//Para a tabela de Serviço, criar um método get para buscar todos os métodos atribuídos a um determinado funcionário a partir de determinada data #82
+	@Query(value = "select * from servico WHERE id_funcionario = :idFuncionario and (data_entrada between :dataInicio and CURRENT_DATE ()) and status = 'CONCLUIDO'", nativeQuery = true)
+	List<Servico> card57(Integer idFuncionario, Date dataInicio);
 }
+
+
