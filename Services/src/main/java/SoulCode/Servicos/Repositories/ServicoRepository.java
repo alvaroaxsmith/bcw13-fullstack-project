b42d2d76d427a7399816e1993c1af06e39831091
giovaner10 <giovanerr10@gmail.com>
2022-04-21 22:56:51 -0300

    cards 52 até 57. ServicoRepository.java.

--- Services/src/main/java/SoulCode/Servicos/Repositories/ServicoRepository.java
+++ Services/src/main/java/SoulCode/Servicos/Repositories/ServicoRepository.java
@@ -11,9 +11,6 @@ import org.springframework.data.jpa.repository.Query;
 public interface ServicoRepository extends JpaRepository<Servico,Integer>{
 
 
-	List<Servico> findBy(Integer idFuncionario, Date data);
-
-
 	//Para a tabela de Serviços, criar um método get pelo Id do Funcionário e a data de Entrada do serviço #77
 	@Query(value = "select * from servico WHERE id_funcionario = :idFuncionario and  data_entrada = :data", nativeQuery = true)
 	List<Servico> card52(Integer idFuncionario, Date data);
@@ -33,7 +30,6 @@ public interface ServicoRepository extends JpaRepository<Servico,Integer>{
 	//Para a tabela de Serviço, criar um método get para buscar todos os serviços concluídos de um determinado funcionário em um determinado períodi #81
 	@Query(value = "select * from servico WHERE id_funcionario = :idFuncionario and (data_entrada between :dataInicio and :dataFinal) and status = 'CONCLUIDO'", nativeQuery = true)
 	List<Servico> card56(Integer idFuncionario, Date dataInicio, Date dataFinal);
-
 
 	//Para a tabela de Serviço, criar um método get para buscar todos os métodos atribuídos a um determinado funcionário a partir de determinada data #82
 	@Query(value = "select * from servico WHERE id_funcionario = :idFuncionario and (data_entrada between :dataInicio and CURRENT_DATE ()) and status = 'ATRIBUIDO'", nativeQuery = true)
